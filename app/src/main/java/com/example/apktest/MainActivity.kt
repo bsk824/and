2f3ac3aceb9affa7aa8610324f4b7fd99cd94a6c
Seungki Baek <bsk824@gmail.com>
2023-01-06 12:57:41 +0900

    새로 고침 뒤로 가기

--- app/src/main/java/com/example/apktest/MainActivity.kt
+++ app/src/main/java/com/example/apktest/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +15,24 @@ class MainActivity : AppCompatActivity() {
         myWebView.settings.javaScriptEnabled = true;
         myWebView.webViewClient = WebViewClient();
 //        myWebView.loadUrl("file:///android_asset/www/index.html");
-        myWebView.loadUrl("https://mcp.hanwhalife.com/");
+        myWebView.loadUrl("http://10.80.100.90:10/");
         myWebView.getSettings().setTextZoom(100);
         myWebView.getSettings().setUserAgentString(myWebView.getSettings().getUserAgentString() + " app");
         WebView.setWebContentsDebuggingEnabled(true);
 
+        var swipe = findViewById<SwipeRefreshLayout>(R.id.swiperefreshlayout)
+        swipe.setOnRefreshListener {
+            myWebView.reload()
+            swipe.isRefreshing = false
+        }
+    }
+
+    override fun onBackPressed() {
+        var myWebView: WebView = findViewById(R.id.webview);
+        if(myWebView.canGoBack()) {
+            myWebView.goBack()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
